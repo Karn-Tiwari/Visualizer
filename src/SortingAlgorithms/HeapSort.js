324abe2eb0ac2d94e8cf201ffee7ae8caa06c371
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2024-09-19 17:42:17 +0530

    Update HeapSort.js

--- src/SortingAlgorithms/HeapSort.js
+++ src/SortingAlgorithms/HeapSort.js
@@ -1,56 +1,30 @@
-export function getHeapSortAnimations(array) {
-    let animations = [];
+export function getBubbleSortAnimations(array) {
+    let animations  = [];
     let auxillaryArray = array.slice();
-    heapSort(auxillaryArray, animations);
+    bubbleSort(auxillaryArray, animations);
     const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
-    console.log("sort works correctly? ", arraysAreEqual(javaScriptSortedArray, auxillaryArray));
-    return [animations, auxillaryArray];
+    console.log("sort works correctly? ",arraysAreEqual(javaScriptSortedArray, auxillaryArray));
+    array = auxillaryArray;
+    return [animations, array];
 }
 
-function heapSort(auxillaryArray, animations) {
+function bubbleSort(auxillaryArray, animations) {
     const N = auxillaryArray.length;
-
-    // Build heap (rearrange array)
-    for (let i = Math.floor(N / 2) - 1; i >= 0; i--) {
-        heapify(auxillaryArray, N, i, animations);
-    }
-
-    // One by one extract an element from heap
-    for (let i = N - 1; i > 0; i--) {
-        animations.push(["comparision1", 0, i]); // Color change for comparison
-        animations.push(["comparision2", 0, i]); // Color change for comparison
-        animations.push(["swap", 0, auxillaryArray[i]]);
-        animations.push(["swap", i, auxillaryArray[0]]);
-        swap(auxillaryArray, 0, i);
-        heapify(auxillaryArray, i, 0, animations);
-    }
-}
-
-function heapify(auxillaryArray, N, i, animations) {
-    let largest = i; // Initialize largest as root
-    let left = 2 * i + 1; // left = 2*i + 1
-    let right = 2 * i + 2; // right = 2*i + 2
-
-    // If left child is larger than root
-    if (left < N && auxillaryArray[left] > auxillaryArray[largest]) {
-        animations.push(["comparision1", left, largest]); // Color change for comparison
-        animations.push(["comparision2", left, largest]); // Color change for comparison
-        largest = left;
-    }
-
-    // If right child is larger than largest so far
-    if (right < N && auxillaryArray[right] > auxillaryArray[largest]) {
-        animations.push(["comparision1", right, largest]); // Color change for comparison
-        animations.push(["comparision2", right, largest]); // Color change for comparison
-        largest = right;
-    }
-
-    // If largest is not root
-    if (largest !== i) {
-        animations.push(["swap", i, auxillaryArray[largest]]);
-        animations.push(["swap", largest, auxillaryArray[i]]);
-        swap(auxillaryArray, i, largest);
-        heapify(auxillaryArray, N, largest, animations);
+    let iters = N - 1;
+    while(iters > 0) {
+        let swapped = false;
+        for(let i = 0; i < iters; ++i) {
+            animations.push(["comparision1", i, i + 1]);
+            animations.push(["comparision2", i, i + 1]);
+            if(auxillaryArray[i] > auxillaryArray[i + 1]) {
+                swapped = true;
+                animations.push(["swap", i, auxillaryArray[i + 1]]);
+                animations.push(["swap", i + 1, auxillaryArray[i]]);
+                swap(auxillaryArray, i, i + 1);
+            }
+        }
+        if(swapped === false) break;
+        iters--;
     }
 }
 
@@ -65,9 +39,9 @@ function arraysAreEqual(firstArray, secondArray) {
         return false;
     }
     for (let i = 0; i < firstArray.length; i++) {
-        if (firstArray[i] !== secondArray[i]) {
-            return false;
-        }
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
     }
     return true;
 }
